478360b652a876613b42d095f2c7c8a13a4552be
oscar_blkd <oscar.belkadi@etu.univ-amu.fr>
2025-03-03 17:58:01 +0100

    Premiere scene découverte

---
+++ yellow_submarine/src/components/YellowSubmarine.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <canvas></canvas>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Test } from '@/YellowSubmarine/TestScene';
+
+export default defineComponent({
+  name: 'YellowSubmarine',
+  props: {
+    msg: String,
+  },
+  mounted() {
+      const canvas = document.querySelector("canvas")!;
+      new Test(canvas);
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas{
+  width: 100%;
+  height: 100%;
+}
+</style>
