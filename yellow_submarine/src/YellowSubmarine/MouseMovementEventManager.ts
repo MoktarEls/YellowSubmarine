b972949068c2cafb78f030e73d09141c36d03b41
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-05-06 20:44:40 +0200

    updated MouseMovementEventManager.ts

--- yellow_submarine/src/YellowSubmarine/MouseMovementEventManager.ts
+++ yellow_submarine/src/YellowSubmarine/MouseMovementEventManager.ts
@@ -1,19 +1,21 @@
 import {Scene} from "@babylonjs/core";
+import {GameOld} from "@/YellowSubmarine/GameOld";
 
 type MouseMovementAction = (
-    deltaXPixel: number,
-    deltaYPixel: number,
-    deltaXRatio: number,
-    deltaYRatio: number
+    movementX: number,
+    movementY: number,
 ) => void;
 
 export class MouseMovementEventManager {
 
     private static _scene: Scene;
     private static _mouseMovementActions: Set<MouseMovementAction> = new Set();
+    private static _isPointerLocked = false;
 
     constructor(scene: Scene) {
         MouseMovementEventManager._scene = scene;
+        MouseMovementEventManager.handlePointerLocking();
+        MouseMovementEventManager.activateMouseMovementEventTrigger();
     }
 
     public static registerMouseMovement(action: MouseMovementAction, context?: any){
@@ -26,5 +28,30 @@ export class MouseMovementEventManager {
         this._mouseMovementActions.delete(action);
     }
 
+    private static activateMouseMovementEventTrigger(){
+        this._scene.onPointerObservable.add((pointerInfo) => {
+            const event = pointerInfo.event as PointerEvent;
+            const movementX = event.movementX;
+            const movementY = event.movementY;
+            if(movementX !== 0 || movementY !== 0){
+                this.notifyMouseMovement(movementX, movementY);
+            }
+        })
+    }
 
+    private static handlePointerLocking() {
+        const canvas = GameOld.engine.getRenderingCanvas();
+        if (canvas) {
+            canvas.addEventListener("click", () => {
+                canvas.requestPointerLock();
+            });
+            document.addEventListener("pointerlockchange", () => {
+                this._isPointerLocked = document.pointerLockElement === canvas;
+            });
+        }
+    }
+
+    private static notifyMouseMovement(movementX: number, movementY: number) {
+        this._mouseMovementActions.forEach(action =>  action(movementX, movementY) );
+    }
 }
