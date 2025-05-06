63a84ed61960f5282075012560e75e22f72175a4
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-05-06 23:18:12 +0200

    Game updated

--- yellow_submarine/src/YellowSubmarine/Game.ts
+++ yellow_submarine/src/YellowSubmarine/Game.ts
@@ -1,59 +1,24 @@
-import {Color3, Engine, Scene, ShaderMaterial, Vector3} from "@babylonjs/core";
 import {World} from "@/YellowSubmarine/World";
+import {GameEngine} from "@/YellowSubmarine/GameEngine";
 
-export class Game {
+export class Game{
 
     private static _world: World;
-    private static _worldScene: Scene;
-    private static _engine : Engine;
-    private static _canvas: HTMLCanvasElement;
+    private static _gameEngine: GameEngine;
 
-
-    public static get worldScene(): Scene {
-        return this._worldScene;
-    }
-    public static get engine(): Engine {
-        return this._engine;
-    }
-    public static get canvas(): HTMLCanvasElement {
-        return this._canvas;
+    constructor(canvas: HTMLCanvasElement) {
+        Game._gameEngine = new GameEngine(canvas);
+        Game._world = new World(Game._gameEngine);
     }
 
-    public static get world(): World {
-        return this._world;
-    }
-
-    constructor(canvas: HTMLCanvasElement){
-        Game._canvas = canvas;
-        Game._engine = new Engine(canvas, true);
-        Game._worldScene = new Scene(Game._engine);
-        Engine.ShadersRepository = "../shaders/";
-        Game._world = new World(Game._worldScene);
-        Game._world.initialize();
-
-        const shader = new ShaderMaterial('shader', Game._worldScene, {
-            vertex : "shader",
-            fragment : "shader",
-        }, {
-            attributes: ['position', 'normal', 'uv'],
-            uniforms: ['world', 'worldView', 'worldViewProjection'],
-        })
-
-        shader.setVector3('vLightPosition', Game._world.getSun().getPosition());
-        shader.setVector3('vColor', new Vector3(0.5, 0.5, 0.5));
-
-        Game._worldScene.meshes.forEach(mesh => {
-            if (!(mesh.name === "sun" || mesh.name === "sunHalo" || mesh.name === "waterPlane")) {
-                mesh.material = shader;
-                console.log(mesh.name);
-                //shader.setTexture('textureSampler', mesh.material.diffuseTexture);
-            }
-        })
-
-        Game._engine.runRenderLoop(() => {
-            World.scene.render()
+    public static registerUpdateAction(
+        action: (deltaTimeInSeconds: number) => void,
+        context?: any
+    ){
+        const boundAction = context ? action.bind(context) : action;
+        this._world.onBeforeRenderObservable.add(() => {
+            boundAction(GameEngine.getDeltaTimeInSeconds());
         })
     }
-
 
 }
