fc491e5377c873676c29b452d390b0a39c880afe
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-05-12 19:28:59 +0200

    Sea is fixed maybe

--- yellow_submarine/src/YellowSubmarine/Sea.ts
+++ yellow_submarine/src/YellowSubmarine/Sea.ts
@@ -1,4 +1,4 @@
-    import {Color3, Mesh, MeshBuilder, Texture, Vector2, Vector3} from "@babylonjs/core";
+    import {Color3, Mesh, MeshBuilder, Scene, Texture, Vector2, Vector3} from "@babylonjs/core";
     import {WaterMaterial} from "@babylonjs/materials";
     //import {ToonWaterAndProbeMaterial} from "@/YellowSubmarine/shader material/ToonWaterAndProbeMaterial";
     import {World} from "@/YellowSubmarine/World";
@@ -10,11 +10,8 @@
         private static _instance: Sea;
         private _groundMesh: Mesh;
 
-        constructor(private _world: World) {
-            this._groundMesh = new Mesh("");
-        }
-
-        public init(): void {
+        constructor(private _scene: Scene) {
+            Sea._instance = this;
             this._groundMesh = MeshBuilder.CreateGround(
                 "waterPlane",
                 {
@@ -22,29 +19,9 @@
                     height: 512,
                     subdivisions: 64,
                 },
-                this._world.scene
+                this._scene
             );
-
-            /*const waterMaterial = new WaterMaterial("seaMaterial", this._world.scene, new Vector2(512,512));
-            waterMaterial.bumpTexture = new Texture("/textures/noiseTexture.png", this._world.scene);
-
-            waterMaterial.windForce = -15;
-            waterMaterial.waveHeight = 0;
-            waterMaterial.windDirection = new Vector2(1, 1);
-            waterMaterial.waterColor = new Color3(0.1, 0.1, 0.6);
-            waterMaterial.colorBlendFactor = 0.3;
-            waterMaterial.bumpHeight = 0.01;
-            waterMaterial.waveLength = 0.1;
-            waterMaterial.alpha = 0.3;
-
-            waterMaterial.addToRenderList(this._world.sun.sunMesh);
-            waterMaterial.addToRenderList(this._world.sun.haloMesh);
-            waterMaterial.addToRenderList(this._world.skybox.mesh);
-            waterMaterial.addToRenderList(this._world.submarine.mesh);*/
-
             this._groundMesh.material = ToonWaterAndProbeMaterial.material;
-            // this._groundMesh.material = ToonWaterMaterial.material;
-            // ToonWaterAndProbeMaterial.applyToMesh(this._groundMesh);
         }
 
     }
