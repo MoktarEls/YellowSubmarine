934ea2f4fdea27df13e2c4c6e459a6c28d16f04b
e21205621 <moktar.el-elsherif@etu.univ-amu.fr>
2025-05-17 16:34:55 +0200

    we have a method, but not the best one yet

--- yellow_submarine/src/YellowSubmarine/keyzone system/KeyZoneFactory.ts
+++ yellow_submarine/src/YellowSubmarine/keyzone system/KeyZoneFactory.ts
@@ -1,6 +1,6 @@
 ﻿import {KeyZone} from "@/YellowSubmarine/keyzone system/KeyZone";
 import {SphericDetectionZone} from "@/YellowSubmarine/detection system/SphericDetectionZone";
-import {MeshBuilder, Vector3} from "@babylonjs/core";
+import {LoadAssetContainerAsync, Mesh, MeshBuilder, Vector3} from "@babylonjs/core";
 import {Game} from "@/YellowSubmarine/Game";
 
 export class KeyZoneFactory {
@@ -19,7 +19,45 @@ export class KeyZoneFactory {
 
         island.mesh.position = new Vector3(0, 0, 40);
 
+        this.loadMesh();
         return island;
+    }
+
+    private static loadMesh(): void {
+        LoadAssetContainerAsync("models/dolphinIsland.glb", Game.scene)
+            .then( (container) => {
+
+            const island = container.transformNodes[0];
+            const pedro = container.transformNodes[1];
+
+            const pedroPosition = pedro.position;
+            pedro.position = Vector3.Zero();
+
+            island.parent = null;
+            pedro.parent = null;
+
+            const islandMeshArray = island.getChildMeshes<Mesh>();
+            const islandMesh = Mesh.MergeMeshes(islandMeshArray, true, undefined, undefined, undefined, true);
+
+            const pedroMeshArray = pedro.getChildMeshes<Mesh>();
+            const pedroMesh = Mesh.MergeMeshes(pedroMeshArray, true, undefined, undefined, undefined, true);
+
+            if(islandMesh){
+                console.log(islandMesh.position);
+            }
+
+            if(pedroMesh){
+                pedroMesh.position = pedroPosition;
+                const detectionZone = new SphericDetectionZone(10, true);
+                detectionZone.zone.parent = pedroMesh;
+
+                console.log("trasnform Node", pedro.position);
+                console.log("absolute", pedroMesh.absolutePosition);
+                console.log("relative", pedroMesh.position);
+            }
+
+
+        });
     }
 
 }
