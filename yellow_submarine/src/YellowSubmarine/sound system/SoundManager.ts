27dc741b372087c71e3ebff19893beba1bab3950
oscar_blkd <oscar.belkadi@etu.univ-amu.fr>
2025-05-23 09:34:01 +0200

    Son qui apparait moins brute

--- yellow_submarine/src/YellowSubmarine/sound system/SoundManager.ts
+++ yellow_submarine/src/YellowSubmarine/sound system/SoundManager.ts
@@ -4,6 +4,9 @@ import {
     CreateSoundAsync, ISoundOptions,
     StaticSound
 } from "@babylonjs/core";
+import {name} from "@babylonjs/gui";
+import * as url from "node:url";
+import * as url from "node:url";
 
 export class SoundManager {
 
@@ -12,11 +15,15 @@ export class SoundManager {
     private _SFXsounds = new Map<string, StaticSound>();
     private _UIsounds = new Map<string, StaticSound>();
     private _MUSICsounds = new Map<string, StaticSound>();
-
-
+    private _SFXVolume : number;
+    private _UIVolume : number;
+    private _MUSICVolume : number;
 
     constructor() {
         SoundManager.instance = this;
+        this._SFXVolume = 1;
+        this._UIVolume = 1;
+        this._MUSICVolume = 1;
     }
 
     private async load(name: string, url: string, options?: Partial<ISoundOptions>) {
@@ -28,13 +35,14 @@ export class SoundManager {
         const sound = map.get(name);
         if(sound) {
             if(options?.spatialSound && mesh) sound.spatial.attach(mesh)
+            this.fadeIn(sound, options?.volume ?? 1, 50);
             sound.play();
         }
         else{
             await this.load(name, url, options).then((sound) => {
                 map.set(name, sound)
                 if(options?.spatialSound && mesh) sound.spatial.attach(mesh)
-                sound.volume = 0.2;
+                this.fadeIn(sound, options?.volume ?? 1, 50);
                 sound.play();
             });
         }
@@ -52,15 +60,82 @@ export class SoundManager {
         await this.play(name, "sounds/music", this._MUSICsounds, options, mesh);
     }
 
+    private fadeIn(sound: StaticSound, targetVolume: number, duration: number) {
+        sound.volume = 0;
+        const stepTime = 20;
+        const steps = duration / stepTime;
+        let currentStep = 0;
+
+        const interval = setInterval(() => {
+            currentStep++;
+            const volume = targetVolume * (currentStep / steps);
+            sound.volume = Math.min(volume, targetVolume);
+            if (currentStep >= steps) clearInterval(interval);
+        }, stepTime);
+    }
+
+    private fadeOutAndStop(sound: StaticSound, duration: number) {
+        const stepTime = 20;
+        const steps = duration / stepTime;
+        let currentStep = 0;
+        const initialVolume = sound.volume;
+
+        const interval = setInterval(() => {
+            currentStep++;
+            const volume = initialVolume * (1 - currentStep / steps);
+            sound.volume = Math.max(volume, 0);
+            if (currentStep >= steps) {
+                clearInterval(interval);
+                sound.stop();
+            }
+        }, stepTime);
+    }
+
     public stopSFX(name: string) {
-        this._SFXsounds.get(name)?.stop();
+        const sound = this._SFXsounds.get(name);
+        if (sound) this.fadeOutAndStop(sound, 50);
     }
 
     public stopUI(name: string) {
-        this._UIsounds.get(name)?.stop();
+        const sound = this._UIsounds.get(name);
+        if (sound) this.fadeOutAndStop(sound, 50);
     }
 
     public stopMUSIC(name: string) {
-        this._MUSICsounds.get(name)?.stop();
+        const sound = this._MUSICsounds.get(name);
+        if (sound) this.fadeOutAndStop(sound, 50);
+    }
+
+    public get SFXVolume(): number {
+        return this._SFXVolume;
+    }
+
+    public set SFXVolume(value: number) {
+        this._SFXVolume = value;
+        this._SFXsounds.forEach((sound) => {
+            sound.volume = this._SFXVolume;
+        })
+    }
+
+    public get UIVolume(): number {
+        return this._UIVolume;
+    }
+
+    public set UIVolume(value: number) {
+        this._UIVolume = value;
+        this._UIsounds.forEach((sound) => {
+            sound.volume = this._UIVolume;
+        })
+    }
+
+    public get MUSICVolume(): number {
+        return this._MUSICVolume;
+    }
+
+    public set MUSICVolume(value: number) {
+        this._MUSICVolume = value;
+        this._MUSICsounds.forEach((sound) => {
+            sound.volume = this._MUSICVolume;
+        })
     }
 }
