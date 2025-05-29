834da1531f00c58bbdc72bbdb44f3b97741dfada
omar-ubuntu <omar.aboudou@etu.univ-amu.fr>
2025-05-29 18:40:21 +0200

    Fixed Flying submarine

--- yellow_submarine/src/YellowSubmarine/Sea.ts
+++ yellow_submarine/src/YellowSubmarine/Sea.ts
@@ -1,6 +1,11 @@
 import {
     Mesh,
     MeshBuilder,
+    PhysicsBody,
+    PhysicsMotionType,
+    PhysicsShape,
+    PhysicsShapeType,
+    Vector3,
 } from "@babylonjs/core";
 import {Game} from "@/YellowSubmarine/Game";
 import {ReflectiveToonWaterMaterial} from "@/YellowSubmarine/shader material/ReflectiveToonWaterMaterial";
@@ -8,11 +13,14 @@ import {ReflectiveToonWaterMaterial} from "@/YellowSubmarine/shader material/Ref
 export class Sea {
 
         private static _instance: Sea;
-        private _groundMesh: Mesh;
+        private _seaMesh: Mesh;
+        private _invisiblePlane: Mesh;
+        private _invisiblePlaneBody: PhysicsBody;
+        private _invisiblePlaneShape: PhysicsShape;
 
         constructor() {
             Sea._instance = this;
-            this._groundMesh = MeshBuilder.CreateGround(
+            this._seaMesh = MeshBuilder.CreateGround(
                 "waterPlane",
                 {
                     width: 2048,
@@ -21,7 +29,29 @@ export class Sea {
                 },
                 Game.scene
             );
-            this._groundMesh.material = ReflectiveToonWaterMaterial.material;
+            this._invisiblePlane = MeshBuilder.CreateGround(
+                "collisionPlane",
+                {
+                    width: 2048,
+                    height: 2048,
+                    subdivisions: 128,
+                },
+                Game.scene
+            );
+            this._invisiblePlane.position = new Vector3(0,-2,0);
+            this._invisiblePlaneBody = new PhysicsBody(this._invisiblePlane, PhysicsMotionType.STATIC, false, Game.scene);
+            this._invisiblePlaneShape = new PhysicsShape({
+                type: PhysicsShapeType.BOX,
+                parameters: {
+                    mesh: this._invisiblePlane,
+                    extents: new Vector3(2048,2,2048),
+                }
+            }, Game.scene);
+            this._invisiblePlaneBody.shape = this._invisiblePlaneShape;
+            this._invisiblePlaneBody.setMassProperties({
+                mass: 0,
+            });
+            this._seaMesh.material = ReflectiveToonWaterMaterial.material;
 
         }
 
