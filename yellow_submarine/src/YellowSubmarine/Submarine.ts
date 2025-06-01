4ba0cb23fd04d47700f4a9f0bee17c449c7f6553
e21205621 <moktar.el-elsherif@etu.univ-amu.fr>
2025-06-01 12:17:24 +0200

    updated speed

--- yellow_submarine/src/YellowSubmarine/Submarine.ts
+++ yellow_submarine/src/YellowSubmarine/Submarine.ts
@@ -30,7 +30,7 @@ export class Submarine {
     private static _instance: Submarine;
     private _mesh!: AbstractMesh;
 
-    private _movementForce = 2500000;
+    private _movementForce = 1250000;
 
     private _rotationForce = 300000;
     private _grappler: Grappler;
