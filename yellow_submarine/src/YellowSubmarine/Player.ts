725265e84353ede398fcb6a4b341dfd2611f463e
OmrAbd <om4r.abou@gmail.com>
2025-06-05 06:36:07 +0200

    Updated Player.ts to be easier to use with my incoming InteractionManager.ts

--- yellow_submarine/src/YellowSubmarine/Player.ts
+++ yellow_submarine/src/YellowSubmarine/Player.ts
@@ -1,5 +1,5 @@
 import {Game} from "@/YellowSubmarine/Game";
-import {Angle, KeyboardEventTypes, KeyboardInfo, Observable, Scalar} from "@babylonjs/core";
+import {Angle, KeyboardEventTypes, KeyboardInfo, Observable, PointerInfo, Scalar} from "@babylonjs/core";
 import {ConfigurableCamera} from "@/YellowSubmarine/camera system/ConfigurableCamera";
 import {CameraConfiguration} from "@/YellowSubmarine/camera system/CameraConfiguration";
 import {Submarine} from "@/YellowSubmarine/Submarine";
@@ -9,107 +9,94 @@ type CameraRotationInfo = {movementX: number, movementY: number};
 
 export class Player {
 
+    private _playerCameraConfiguration: CameraConfiguration = new CameraConfiguration();
+    private _horizontalCameraSensitivity = 5;
+    private _verticalCameraSensitivity = 5;
+    private _isMovementEnabled = true;
 
-    private static _instance: Player;
-    private static _onCameraRotationObservable: Observable<CameraRotationInfo> = new Observable();
-    private static _onPlayerPressedAKey: Observable<KeyboardInfo> = new Observable();
-    private static _playerCameraConfiguration: CameraConfiguration = new CameraConfiguration();
-    private static _horizontalCameraSensitivity = 5;
-    private static _verticalCameraSensitivity = 5;
-    private static _isMovementEnabled = true;
+    private _onAnyKeyIsPressedObservable: Observable<KeyboardInfo> = new Observable();
+    private _onMouseWheelScrolledObservable: Observable<WheelEvent> = new Observable();
+    private _onPointerObservable: Observable<PointerInfo> = new Observable();
 
+    private _isForwardPressed = false;
+    private _isBackwardPressed = false;
+    private _isLeftPressed = false;
+    private _isRightPressed = false;
 
-    public static get instance() {
-        return this._instance;
-    }
-
-    public static get onCameraRotationObservable(){
-        return this._onCameraRotationObservable;
-    }
-
-    public static get onPlayerPressedAKey(): Observable<KeyboardInfo> {
-        return this._onPlayerPressedAKey;
-    }
-
-    public static get playerCameraConfiguration(): CameraConfiguration {
+    public get playerCameraConfiguration(): CameraConfiguration {
         return this._playerCameraConfiguration;
     }
 
-    private static _isForwardPressed = false;
-    private static _isBackwardPressed = false;
-    private static _isLeftPressed = false;
-    private static _isRightPressed = false;
-
     constructor() {
-        Player._instance = this;
-        Player.initializeCameraParameter();
-        Player.registerKeyboardInputs();
-        Player.registerMouseMovementInputs();
-        Conversation.onAnyConversationStart.add(() => Player.disableMovement());
-        Conversation.onAnyConversationEnd.add(() => Player.enableMovement());
+        this.initializeCameraParameter();
+        this.registerKeyboardInputs();
+        this.registerPointerInputs();
+        this.registerMouseWheelInputs();
+        Conversation.onAnyConversationStart.add(() => this.disableMovement());
+        Conversation.onAnyConversationEnd.add(() => this.enableMovement());
     }
 
-    public static isMoveForwardPressed(): boolean {
+    public isMoveForwardPressed(): boolean {
         return this._isForwardPressed && this._isMovementEnabled;
     }
 
-    public static isMoveBackwardPressed(): boolean {
+    public isMoveBackwardPressed(): boolean {
         return this._isBackwardPressed && this._isMovementEnabled;
     }
 
-    public static isTurnLeftPressed(): boolean {
+    public isTurnLeftPressed(): boolean {
         return this._isLeftPressed && this._isMovementEnabled;
     }
 
-    public static isTurnRightPressed(): boolean {
+    public isTurnRightPressed(): boolean {
         return this._isRightPressed && this._isMovementEnabled;
     }
 
-    private static registerKeyboardInputs(){
-        const scene = Game.scene;
-        scene.onKeyboardObservable.add( (eventData) => {
-            if(Game.isGameFocused){
-                const state = eventData.type === KeyboardEventTypes.KEYDOWN;
-                if(eventData.event.code === "KeyW"){
-                    this._isForwardPressed = state;
-                }
-                else if(eventData.event.code === "KeyS"){
-                    this._isBackwardPressed = state;
-                }
-                else if(eventData.event.code === "KeyA"){
-                    this._isLeftPressed = state;
-                }
-                else if(eventData.event.code === "KeyD"){
-                    this._isRightPressed = state;
-                }
-                this.onPlayerPressedAKey.notifyObservers(eventData);
-
-            }
-        } );
+    public enableMovement(){
+        this._isMovementEnabled = true;
     }
 
-    private static registerMouseMovementInputs() {
+    public disableMovement(){
+        this._isMovementEnabled = false;
+    }
+
+    private registerKeyboardInputs(){
+        const scene = Game.scene;
+        scene.onKeyboardObservable.add( (keyboardInfo) => {
+            if(Game.isGameFocused){
+                const state = keyboardInfo.type === KeyboardEventTypes.KEYDOWN;
+                switch(keyboardInfo.event.code){
+                    case "KeyW":
+                        this._isForwardPressed = state;
+                        break;
+                    case "KeyS":
+                        this._isBackwardPressed = state;
+                        break;
+                    case "KeyA":
+                        this._isLeftPressed = state;
+                        break;
+                    case "KeyD":
+                        this._isRightPressed = state;
+                        break;
+
+                }
+                this._onAnyKeyIsPressedObservable.notifyObservers(keyboardInfo);
+            }
+        });
+
+    }
+
+    private registerPointerInputs() {
         const scene = Game.scene;
         scene.onPointerObservable.add((pointerInfo) => {
             if(Game.isGameFocused){
-                const event = pointerInfo.event as PointerEvent;
-                const movementX = event.movementX/window.screen.width;
-                const movementY = event.movementY/window.screen.height;
-                this._onCameraRotationObservable.notifyObservers({movementX, movementY});
-                this.updateCameraParameter(movementX, movementY);
+                this._onPointerObservable.notifyObservers(pointerInfo);
+                this.updateCameraParameter(pointerInfo);
             }
         })
     }
 
-    public static enableMovement(){
-        this._isMovementEnabled = true;
-    }
-
-    public static disableMovement(){
-        this._isMovementEnabled = false;
-    }
-
-    private static initializeCameraParameter() {
+    private initializeCameraParameter() {
         Submarine.instance.meshCreationPromise.then((mesh) => {
             this._playerCameraConfiguration.target = mesh;
             this._playerCameraConfiguration.distanceFromTarget = 25;
@@ -121,13 +108,21 @@ export class Player {
         })
     }
 
-    private static updateCameraParameter(movementX: number, movementY: number) {
+    private updateCameraParameter(pointerInfo: PointerInfo) {
+        const event = pointerInfo.event as PointerEvent;
+        const movementX = event.movementX/window.screen.width;
+        const movementY = event.movementY/window.screen.height;
         this._playerCameraConfiguration.wantedAlpha = Scalar.LerpAngle(this._playerCameraConfiguration.wantedBeta, this._playerCameraConfiguration.wantedAlpha - movementX * this._horizontalCameraSensitivity, 1);
         this._playerCameraConfiguration.wantedBeta = Scalar.LerpAngle(this._playerCameraConfiguration.wantedBeta, this._playerCameraConfiguration.wantedBeta - movementY * this._verticalCameraSensitivity, 1);
 
     }
 
-    private static isCurrentCamera(){
-        return ConfigurableCamera.instance.cameraConfiguration == this._playerCameraConfiguration;
+    private registerMouseWheelInputs() {
+        const canvas = Game.canvas;
+        canvas.addEventListener("wheel", (event: WheelEvent) => {
+            if(Game.isGameFocused){
+                this._onMouseWheelScrolledObservable.notifyObservers(event);
+            }
+        });
     }
 }
