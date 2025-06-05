d42479676d44d6884fdcade676da31189bfffb1e
OmrAbd <om4r.abou@gmail.com>
2025-06-05 06:49:52 +0200

    Turned AbstractInteraction into a state like class

--- yellow_submarine/src/YellowSubmarine/interaction system/interactions/AbstractInteraction.ts
+++ yellow_submarine/src/YellowSubmarine/interaction system/interactions/AbstractInteraction.ts
@@ -1,31 +1,70 @@
 import {InteractionManager} from "@/YellowSubmarine/interaction system/InteractionManager";
+import {Observable} from "@babylonjs/core";
 
 export abstract class AbstractInteraction {
 
-    protected _code:string;
-    protected  _simplifiedCode: string;
+    private _onAvailableObservable: Observable<void> = new Observable();
+    private _onUnavailableObservable: Observable<void> = new Observable();
+    private _onStartObservable: Observable<void> = new Observable();
+    private _onEndObservable: Observable<void> = new Observable();
 
-    public get code(): string {
+    get code(): string {
         return this._code;
     }
 
-    public get simplifiedCode(): string {
+    get simplifiedCode(): string {
         return this._simplifiedCode;
     }
 
-    protected constructor(code: string, simplifiedCode: string) {
-        this._code = code;
-        this._simplifiedCode = simplifiedCode;
+    get onAvailableObservable(): Observable<void> {
+        return this._onAvailableObservable;
     }
 
+    get onUnavailableObservable(): Observable<void> {
+        return this._onUnavailableObservable;
+    }
+
+    get onStartObservable(): Observable<void> {
+        return this._onStartObservable;
+    }
+
+    get onEndObservable(): Observable<void> {
+        return this._onEndObservable;
+    }
+
+    protected constructor(private _interactionManager: InteractionManager, protected _code: string, protected _simplifiedCode: string) {}
+
     public makeAvailable(){
-        InteractionManager.instance.makeInteractionAvailable(this);
+        this._interactionManager.addToAvailableInteraction(this);
     }
 
     public makeUnavailable(){
-        InteractionManager.instance.makeInteractionUnavailable(this);
+        this._interactionManager.removeFromAvailableInteraction(this);
     }
 
-    public abstract executeInteraction(): void;
+    public onAvailable(){
+        this._onAvailable();
+        this._onAvailableObservable.notifyObservers();
+    }
+
+    public onUnavailable(){
+        this._onUnavailable();
+        this._onUnavailableObservable.notifyObservers();
+    }
+
+    public onStart(){
+        this._onStart();
+        this._onStartObservable.notifyObservers();
+    }
+
+    public onEnd(){
+        this._onEnd();
+        this._onEndObservable.notifyObservers();
+    }
+
+    protected abstract _onAvailable(): void;
+    protected abstract _onUnavailable(): void;
+    protected abstract _onStart(): void;
+    protected abstract _onEnd(): void;
 
 }
