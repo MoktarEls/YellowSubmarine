55edc3455dc511eb2c23a6918c1fa0a83ae40171
OmrAbd <om4r.abou@gmail.com>
2025-06-05 11:44:32 +0200

    Starting implementing new dialogue nodes

--- yellow_submarine/src/YellowSubmarine/dialogue system/nodes/AbstractDialogueNode.ts
+++ yellow_submarine/src/YellowSubmarine/dialogue system/nodes/AbstractDialogueNode.ts
@@ -1,24 +1,9 @@
 ﻿export abstract class AbstractDialogueNode {
 
-    private _text = "NO TEXT";
-    private _nextNode: AbstractDialogueNode | undefined = undefined;
-
-    public get text() : string {
-        return this._text;
-    }
-
-    public set text(text: string) {
-        this._text = text;
-    }
-
-    public set nextNode(value: AbstractDialogueNode | undefined) {
-        this._nextNode = value;
-    }
-
-    public get nextNode(): AbstractDialogueNode | undefined {
-        return this._nextNode;
-    }
+    public abstract get text() : string;
 
     public abstract execute(): void;
+
+    public abstract isFinal(): boolean;
 
 }
--- yellow_submarine/src/YellowSubmarine/dialogue system/nodes/SimpleDialogueNode.ts
+++ yellow_submarine/src/YellowSubmarine/dialogue system/nodes/SimpleDialogueNode.ts
@@ -1,7 +1,23 @@
 ﻿import {AbstractDialogueNode} from "@/YellowSubmarine/dialogue system/nodes/AbstractDialogueNode";
 
-export class SimpleDialogueNode extends AbstractDialogueNode{
+export abstract class SimpleDialogueNode extends AbstractDialogueNode{
 
-    execute(): void {return;}
+    private _nextNode: AbstractDialogueNode | undefined;
+
+    public set nextNode(value: AbstractDialogueNode | undefined) {
+        this._nextNode = value;
+    }
+
+    public get nextNode(): AbstractDialogueNode | undefined {
+        return this._nextNode;
+    }
+
+    public execute(): void {
+        return;
+    }
+
+    public isFinal(): boolean {
+        return this.nextNode === undefined;
+    }
 
 }
