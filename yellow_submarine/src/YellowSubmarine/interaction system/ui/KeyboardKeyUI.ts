6c74c0ca3441a8f35238d857c0cf4c51c974aced
OmrAbd <om4r.abou@gmail.com>
2025-06-05 08:58:53 +0200

    implemented a UI for displaying KeyboardKeys

---
+++ yellow_submarine/src/YellowSubmarine/interaction system/ui/KeyboardKeyUI.ts
@@ -0,0 +1,32 @@
+import {UI} from "@/YellowSubmarine/ui system/UI";
+import {Control, Rectangle, TextBlock} from "@babylonjs/gui";
+
+export class KeyboardKeyUI extends UI{
+
+    private _rectangle: Rectangle;
+    private _textBlock: TextBlock;
+
+    constructor(private _simplifiedCode: string) {
+        super();
+        this._rectangle = new Rectangle();
+        this._rectangle.width = "80px";
+        this._rectangle.height = "70px";
+        this._rectangle.cornerRadius = 60;
+        this._rectangle.color = "rgb(168, 98, 68)";
+        this._rectangle.thickness = 4;
+        this._rectangle.background = "rgb(255, 255, 255)";
+
+        this._textBlock = new TextBlock();
+        this._textBlock.fontSize = 45;
+        this._textBlock.text = `${_simplifiedCode}`;
+        this._textBlock.fontWeight = "bold";
+        this._textBlock.paddingTop = "6px";
+        this._rectangle.addControl(this._textBlock);
+
+    }
+
+    get controlNode(): Control {
+        return this._rectangle;
+    }
+
+}
