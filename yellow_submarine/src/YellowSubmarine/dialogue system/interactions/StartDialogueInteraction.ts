8d602688bf01b55a13401c5331f3ce8ebeaf392c
OmrAbd <om4r.abou@gmail.com>
2025-06-07 13:03:55 +0200

    ...

---
+++ yellow_submarine/src/YellowSubmarine/dialogue system/interactions/StartDialogueInteraction.ts
@@ -0,0 +1,24 @@
+﻿import {Dialogue} from "@/YellowSubmarine/dialogue system/Dialogue";
+import {WorldInteraction} from "@/YellowSubmarine/world interaction system/interaction/WorldInteraction";
+import {AbstractMesh} from "@babylonjs/core";
+
+export class StartDialogueInteraction extends WorldInteraction{
+
+    public get description(): string {
+        return `Parler`;
+    }
+
+    constructor(private _dialogue: Dialogue) {
+        super();
+    }
+
+    get mesh(): AbstractMesh | undefined {
+        return this._dialogue.dialogueProvider?.mesh;
+    }
+
+    protected _start(): void {
+        this._dialogue.startDialogue();
+        this.endOnNextFrame();
+    }
+
+}
