fc4e3db34d1e49260c306146fb7335beb44609d1
OmrAbd <om4r.abou@gmail.com>
2025-06-12 14:35:32 +0200

    Reimplemented AbstractInteraction

--- yellow_submarine/src/YellowSubmarine/interaction system/interactions/AbstractInteraction.ts
+++ yellow_submarine/src/YellowSubmarine/interaction system/interactions/AbstractInteraction.ts
@@ -3,75 +3,25 @@ import {Game} from "@/YellowSubmarine/Game";
 
 export abstract class AbstractInteraction {
 
-    private _onAvailableObservable: Observable<void> = new Observable();
-    private _onUnavailableObservable: Observable<void> = new Observable();
-    private _onStartedObservable: Observable<void> = new Observable();
-    private _onEndedObservable: Observable<void> = new Observable();
+    private _onStarted = new Observable<void>()
+    private _onEnded = new Observable<void>();
 
-    get code(): string {
-        return this._code;
+    public get onStarted(){
+        return this._onStarted;
     }
 
-    get simplifiedCode(): string {
-        return this._simplifiedCode;
-    }
-
-    get onAvailableObservable(): Observable<void> {
-        return this._onAvailableObservable;
-    }
-
-    get onUnavailableObservable(): Observable<void> {
-        return this._onUnavailableObservable;
-    }
-
-    get onStartedObservable(): Observable<void> {
-        return this._onStartedObservable;
-    }
-
-    get onEndedObservable(): Observable<void> {
-        return this._onEndedObservable;
-    }
-
-    protected _code: string;
-
-    protected _simplifiedCode: string;
-
-    protected constructor(code?: string, simplifiedCode?: string) {
-        this._code = code ?? "";
-        this._simplifiedCode = simplifiedCode ?? "";
-    }
-
-    public onAvailable(){
-        this._onAvailable();
-        this._onAvailableObservable.notifyObservers();
-    }
-
-    public onUnavailable(){
-        this._onUnavailable();
-        this._onUnavailableObservable.notifyObservers();
+    public get onEnded(){
+        return this._onEnded;
     }
 
     public start(){
-        this._start();
-        this._onStartedObservable.notifyObservers();
+        this.onStart();
+        this._onStarted.notifyObservers();
     }
+    protected abstract onStart(): void;
 
-    protected endOnNextFrame(){
-        Game.scene.onBeforeRenderObservable.addOnce(() => {
-            this._end();
-            this._onEndedObservable.notifyObservers();
-        });
-    }
-
-    protected _onAvailable(): void{
-        return;
-    }
-    protected _onUnavailable(): void{
-        return;
-    }
-    protected abstract _start(): void;
-    protected _end(): void{
-        return;
+    protected end(){
+        this._onEnded.notifyObservers();
     }
 
 }
