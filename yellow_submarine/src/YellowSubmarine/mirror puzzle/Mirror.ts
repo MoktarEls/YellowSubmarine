98f76135f8bbffc0dc48eebf297b036faddd6196
OmrAbd <om4r.abou@gmail.com>
2025-06-17 11:26:38 +0200

    ...

--- yellow_submarine/src/YellowSubmarine/mirror puzzle/Mirror.ts
+++ yellow_submarine/src/YellowSubmarine/mirror puzzle/Mirror.ts
@@ -1,8 +1,11 @@
 import {AbstractMesh, Angle, MeshBuilder, TransformNode, Vector3} from "@babylonjs/core";
 import {IReceiveLight} from "@/YellowSubmarine/mirror puzzle/IReceiveLight";
 import {Submarine} from "@/YellowSubmarine/Submarine";
+import {LerpAngle} from "@babylonjs/core/Maths/math.scalar.functions";
 
 export class Mirror implements IReceiveLight {
+
+    private _targetAngleInDegrees;
 
     private static meshToMirrorMap = new Map<AbstractMesh, Mirror>();
     private _transformNode: TransformNode = new TransformNode("MirrorTransformNode");
@@ -25,7 +28,7 @@ export class Mirror implements IReceiveLight {
         return this._mesh;
     }
 
-    constructor(correctAngleInDegress: number) {
+    constructor(correctAngleInDegress: number, defaultAngle: number) {
         this._correctAngleInDegress = correctAngleInDegress;
         const mesh = MeshBuilder.CreateBox("mirrorMesh", {
             width: 2,
@@ -33,6 +36,8 @@ export class Mirror implements IReceiveLight {
             depth: 0.2,
         })
         mesh.parent = this._transformNode;
+        this._transformNode.rotation.y = Angle.FromDegrees(defaultAngle).radians();
+        this._targetAngleInDegrees = defaultAngle;
         Submarine.instance.mirrorDetectionZone.addMeshToDetect(mesh);
         Mirror.meshToMirrorMap.set(mesh, this);
     }
@@ -41,8 +46,8 @@ export class Mirror implements IReceiveLight {
         return Angle.FromRadians(this._transformNode.rotation.y).degrees() === this._correctAngleInDegress;
     }
 
-    public rotate(degrees: number) {
-        throw new Error("Not implemented");
+    public rotate() {
+        this._targetAngleInDegrees += 45;
     }
 
     public get angleInDegrees(){
