e94f98d9def018847d7f5ef628a58ec1b3d79638
OmrAbd <om4r.abou@gmail.com>
2025-06-20 15:30:27 +0200

    Scaled the light beam and repositioned it to be in the center of the mirror

--- yellow_submarine/src/YellowSubmarine/mirror puzzle/MirrorLightBeam.ts
+++ yellow_submarine/src/YellowSubmarine/mirror puzzle/MirrorLightBeam.ts
@@ -9,14 +9,17 @@ export class MirrorLightBeam{
     constructor(private _mirror: Mirror) {
         this._transformNode = new TransformNode("lightBeamTransformNode");
         this._transformNode.parent = this._mirror.mirrorTransformNode;
+        this._transformNode.position = Vector3.Up().scale(4)
 
         this._lightBeamMesh = MeshBuilder.CreateCylinder("lightBeamMesh",{
             height: 5,
             diameter: 1.5,
+            tessellation: 8,
         });
         this._lightBeamMesh.parent = this._transformNode;
         this._lightBeamMesh.rotate(Vector3.Right(), Angle.FromDegrees(90).radians(), Space.WORLD);
         this._lightBeamMesh.position = new Vector3(0,0,2.5);
+        this._lightBeamMesh.scaling = new Vector3(4,1,4)
         this.turnOff();
     }
 
